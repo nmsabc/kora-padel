93d9de86e4d425f3a3145c85e1359ecdfce4ca71
Sebastian Ivan <sebastian.ivan@gmail.com>
2018-06-05 16:36:21 +0300

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,9 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <p>Welcome to Kora Padel and Tennis Timisoara.</p>
+    <p>you can reach us here: +40 722 822 978</p>
+    <Link to="/page-2/">Shop</Link>
   </div>
 )
 
